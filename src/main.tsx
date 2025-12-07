fb4556e776fc5c1c79aa804d4199be8f208db7a3
shakana0 <shakanajamal3@gmail.com>
2025-12-07 19:10:15 +0100

    feat: add ClerkProvider and publisher key

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={clerkFrontendApi}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
