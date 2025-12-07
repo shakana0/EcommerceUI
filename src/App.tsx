adc26d7f86e2caa85618f3d2d1cf04e9875053aa
shakana0 <shakanajamal3@gmail.com>
2025-12-07 19:12:06 +0100

    feat: add rout layout in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import "./App.css";
-import ProductList from "./components/ProductList";
+import "./index.css";
 
-function App() {
-  return <ProductList />;
+import {
+  RouterProvider,
+  createRouter,
+  createRootRoute,
+  createRoute,
+} from "@tanstack/react-router";
+
+import Layout from "./components/Layout/layout";
+
+const rootRoute = createRootRoute({
+  component: Layout,
+});
+
+const router = createRouter({
+  routeTree: rootRoute,
+});
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
