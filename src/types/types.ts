0ea032e1c44808a32e0b61a3f93e96d8b821c179
shakana0 <shakanajamal3@gmail.com>
2025-12-11 16:08:47 +0100

    feat: update types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,22 @@
-export type ProductType = {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-    stockQuantity: number;
-    categoryId: number;
-    categoryName?: string;
-    categoryDescription?: string;
-}
-
 export type Product = {
     name: string;
     description: string;
     price: number;
     stockQuantity: number;
     categoryId: number;
+}
+
+export type ProductData = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stockQuantity: number;
+    category: Category;
+}
+
+type Category = {
+    id: number,
+    name: string,
+    description: string
 }
