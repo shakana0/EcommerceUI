840e2745a77bd0b25f375f5943d0cf387e1c4054
shakana0 <shakanajamal3@gmail.com>
2025-12-11 16:10:37 +0100

    feat: add logic for toast and update class

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut, SignInButton, useAuth } from "@clerk/clerk-react";
 import { useApi } from "../api/api";
 import type { Product } from "../types/types";
+import { toast } from "react-toastify";
 
 export default function Profile() {
   const { post } = useApi();
@@ -8,7 +9,7 @@ export default function Profile() {
 
   async function handleCreateProduct() {
     const newProduct: Product = {
-      name: "2 Gaming Laptop",
+      name: "5 Gaming Laptop",
       description: "High performance laptop for gaming",
       price: 1499.99,
       stockQuantity: 10,
@@ -18,17 +19,30 @@ export default function Profile() {
     try {
       const token = await getToken({ template: "ecom-jwt-template" });
       console.log(token);
+
       const result = await post("/Products", token!, newProduct);
       console.log("Product created:", result);
+
+      toast.success(`Product "${result.name}" created successfully!`, {
+        position: "top-right",
+        autoClose: 3000,
+      });
     } catch (err) {
       console.error("Error creating product:", err);
+
+      toast.error("Failed to create product. Please try again.", {
+        position: "top-right",
+        autoClose: 5000,
+      });
     }
   }
 
   return (
     <>
       <SignedIn>
-        <h1>Your Profile</h1>
+        <h1 className="sm:text-xl lg:text-3xl font-bold text-center">
+          Profile
+        </h1>
         <p>Here is your favorite products...</p>
       </SignedIn>
       <SignedOut>
@@ -36,7 +50,12 @@ export default function Profile() {
         <SignInButton />
       </SignedOut>
 
-      <button onClick={handleCreateProduct}>Save Profile</button>
+      <button
+        className="w-full md:w-auto px-4 py-2 cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-lg"
+        onClick={handleCreateProduct}
+      >
+        Add Product
+      </button>
     </>
   );
 }
