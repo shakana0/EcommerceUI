3ce949afe5416f5c1aead6a1828673fd762c23d3
shakana0 <shakanajamal3@gmail.com>
2025-12-11 16:15:18 +0100

    refactor: update logic

--- src/router.tsx
+++ src/router.tsx
@@ -1,49 +1,36 @@
 // src/router.tsx
-import {
-  createRouter,
-  RouterProvider,
-  Route,
-  RootRoute,
-} from "@tanstack/react-router";
-
+import { createRouter, Route, RootRoute } from "@tanstack/react-router";
 import Layout from "./components/Layout/layout";
 import Landing from "./pages/landing";
 import ProductList from "./pages/productList";
 import Product from "./pages/product";
 import CategoryList from "./pages/categoryList";
 import Profile from "./pages/profile";
-
 import { useAuth } from "@clerk/clerk-react";
 
 function Protected({ children }: { children: React.ReactNode }) {
   const { isSignedIn } = useAuth();
-  if (!isSignedIn) {
-    return <div>Please sign in to access this page.</div>;
-  }
+  if (!isSignedIn) return <div>Please sign in to access this page.</div>;
   return <>{children}</>;
 }
 
-// Root layout
-const rootRoute = new RootRoute({
-  component: Layout,
-});
+const rootRoute = new RootRoute({ component: Layout });
 
-// Child routes
 const landingRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/",
   component: Landing,
 });
 
-const productListRoute = new Route({
+export const productListRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/products",
   component: ProductList,
 });
 
-const productRoute = new Route({
+export const productRoute = new Route({
   getParentRoute: () => rootRoute,
-  path: "/products/$id", // dynamic route
+  path: "/products/$productId",
   component: Product,
 });
 
@@ -63,7 +50,6 @@ const profileRoute = new Route({
   ),
 });
 
-// Create router
 const routeTree = rootRoute.addChildren([
   landingRoute,
   productListRoute,
