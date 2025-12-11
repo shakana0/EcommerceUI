69ef37407d122338717627790bc0c8e0d4533066
shakana0 <shakanajamal3@gmail.com>
2025-12-11 10:41:30 +0100

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./router";
 
 const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkFrontendApi}>
-      <App />
+      <RouterProvider router={router} />
     </ClerkProvider>
   </React.StrictMode>
 );
