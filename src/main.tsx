2c6de38e6092c1cb8c714f46015d6f38fec12e1d
shakana0 <shakanajamal3@gmail.com>
2025-12-11 16:09:31 +0100

    feat: add toast conatiner

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
+import { ToastContainer } from "react-toastify";
 
 const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkFrontendApi}>
       <RouterProvider router={router} />
+      <ToastContainer position="top-right" autoClose={3000} />
     </ClerkProvider>
   </React.StrictMode>
 );
