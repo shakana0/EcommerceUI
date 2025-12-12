4cff4ef6986cf77ae4fa4b046649df9e0d72c22c
shakana0 <shakanajamal3@gmail.com>
2025-12-12 14:07:05 +0100

    feat: add QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkFrontendApi}>
-      <RouterProvider router={router} />
-      <ToastContainer position="top-right" autoClose={3000} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer position="top-right" autoClose={3000} />
+      </QueryClientProvider>
     </ClerkProvider>
   </React.StrictMode>
 );
