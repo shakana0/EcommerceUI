bf4054053797bb63ef01d07342469d693b7a02ca
shakana0 <shakanajamal3@gmail.com>
2025-12-14 11:12:51 +0100

    refactor: remove title

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -6,7 +6,6 @@ export default function Landing() {
       <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold my-6 text-center">
         Welcome to Product Catalog
       </h1>
-      <h1 className="my-8">Products</h1>
       <ProductList />
     </div>
   );
