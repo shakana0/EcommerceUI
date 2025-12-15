ad51162e098b3104608c6c7d0545655f2f271804
shakana0 <shakanajamal3@gmail.com>
2025-12-15 10:26:27 +0100

    chore: add path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
